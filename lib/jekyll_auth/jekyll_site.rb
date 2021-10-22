58fcb03991871f272f9205ba4bcb63c228daf982
Darshan Patil <dapatil211@gmail.com>
2021-10-22 12:50:00 -0400

    simplify pulling notes

--- lib/jekyll_auth/jekyll_site.rb
+++ lib/jekyll_auth/jekyll_site.rb
@@ -11,6 +11,13 @@ class JekyllAuth
         File.join(settings.public_folder, path, "#{fname}.html")
       )
     end
+
+    post '/update' do
+      request.body.rewind
+      data = JSON.parse request.body.read
+      `./pull_notes.sh #{data['pat']}`
+    end
+
     not_found do
       status 404
       four_oh_four = File.expand_path(settings.public_folder + "/404.html", Dir.pwd)
